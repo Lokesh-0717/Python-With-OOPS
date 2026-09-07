5448cc64f930a0d5967f4822e4cd86045a160e0d
Pitla Lokesh <pitlalokesh8a24@gmail.com>
2026-09-07 15:30:18 +0530

    Add files via upload
    Create a Class and Object

---
+++ classobj.py
@@ -0,0 +1,17 @@
+class Books():
+    name="Name of Book"
+    writer="Vlad.K"
+    
+    def Novel(self):
+        print("Name of Novel")
+    def Textbook(self):
+        print("Subject")
+    def Stories(self):
+        print("Ending")
+
+DLS=Books()
+SCN=Books()
+ABH=Books()
+DLS.Stories()
+print(SCN.writer)
+ABH.Textbook()
