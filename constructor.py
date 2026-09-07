abdc67cfc93f5adafc4de0fca5a06ff6aee3b033
Pitla Lokesh <pitlalokesh8a24@gmail.com>
2026-09-07 15:32:47 +0530

    Add files via upload
    Create a Class with a Constructor

---
+++ constructor.py
@@ -0,0 +1,9 @@
+class Sample():
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def Intro(self):
+        print(self.name)
+        print(self.age)
+s1=Sample()
+s1.Intro()
